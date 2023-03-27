b4b56458fcd45131f266ed524f76a226b3451886
m-abirhasan23 <abirhasanjoy07@gmail.com>
2023-03-27 14:22:50 +0600

    hero section working

---
+++ src/data/data.js
@@ -0,0 +1,60 @@
+export const nav=[
+ {
+    name:"menu",
+    path:"/menu"
+ },
+ {
+    name:"reservation",
+    path:"/reservation"
+ },
+ {
+    name:"contact",
+    path:"/contact"
+ },
+ {
+    name:"about",
+    path:"/about"
+ },
+ {
+    name:"blog",
+    path:"/blog"
+ },
+
+]
+
+
+export const aboutData=[
+  
+   {
+
+      imgSrc:"/about-1.jpg"
+   },
+   {
+
+      imgSrc:"/about-2.jpg"
+   },
+   {
+
+      imgSrc:"/about-3.jpg"
+   },
+   {
+
+      imgSrc:"/about-4.jpg"
+   },
+]
+
+
+export const cardData=[
+   {
+      title:"FRESH INGREDIENTS",
+      subtitle:"i've taught myself how to use good, fresh ingredients and to prepare them as simply as possible by cooking only to enhance their intrinsic flavors."
+   },
+   {
+      title:"CHEF'S SPECIALTIES",
+      subtitle:"if you are a chef, no matter how good a chef you are, it's not good cooking for yourself; the joy is in cooking for others - it's the same with music."
+   },
+   {
+      title:"WEDDINGS & EVENTS",
+      subtitle:"one day, in your search for happiness, you discover a partner by your side, and you realize that your happiness has come to help you search"
+   },
+]
