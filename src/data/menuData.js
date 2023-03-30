8c46de5891f011087ddc2273a27d43804800ab85
m-abirhasan23 <abirhasanjoy07@gmail.com>
2023-03-30 23:36:38 +0600

    food menu page building

---
+++ src/data/menuData.js
@@ -0,0 +1,146 @@
+export const menuData=[
+    {
+      "name": "Spaghetti and meatballs",
+      "price": "$12.99",
+      "category": "dinner",
+      "picture": "https://example.com/spaghetti_meatballs.jpg"
+    },
+    {
+      "name": "Tacos",
+      "price": "$3.99",
+      "category": "lunch",
+      "picture": "https://example.com/tacos.jpg"
+    },
+    {
+      "name": "Chicken Alfredo",
+      "price": "$14.99",
+      "category": "dinner",
+      "picture": "https://example.com/chicken_alfredo.jpg"
+    },
+    {
+      "name": "Grilled cheese sandwich",
+      "price": "$6.99",
+      "category": "lunch",
+      "picture": "https://example.com/grilled_cheese.jpg"
+    },
+    {
+      "name": "Fish and chips",
+      "price": "$10.99",
+      "category": "dinner",
+      "picture": "https://example.com/fish_chips.jpg"
+    },
+    {
+      "name": "Beef stir-fry",
+      "price": "$11.99",
+      "category": "dinner",
+      "picture": "https://example.com/beef_stir_fry.jpg"
+    },
+    {
+      "name": "Pad Thai",
+      "price": "$9.99",
+      "category": "dinner",
+      "picture": "https://example.com/pad_thai.jpg"
+    },
+    {
+      "name": "Pizza",
+      "price": "$15.99",
+      "category": "dinner",
+      "picture": "https://example.com/pizza.jpg"
+    },
+    {
+      "name": "Sushi rolls",
+      "price": "$8.99",
+      "category": "lunch",
+      "picture": "https://example.com/sushi_rolls.jpg"
+    },
+    {
+      "name": "Chicken teriyaki",
+      "price": "$12.99",
+      "category": "dinner",
+      "picture": "https://example.com/chicken_teriyaki.jpg"
+    },
+    {
+      "name": "BBQ ribs",
+      "price": "$18.99",
+      "category": "dinner",
+      "picture": "https://example.com/bbq_ribs.jpg"
+    },
+    {
+      "name": "Caesar salad",
+      "price": "$7.99",
+      "category": "lunch",
+      "picture": "https://example.com/caesar_salad.jpg"
+    },
+    {
+      "name": "Roast beef sandwich",
+      "price": "$8.99",
+      "category": "lunch",
+      "picture": "https://example.com/roast_beef_sandwich.jpg"
+    },
+    {
+      "name": "Falafel wrap",
+      "price": "$6.99",
+      "category": "lunch",
+      "picture": "https://example.com/falafel_wrap.jpg"
+    },
+    {
+      "name": "Lentil soup",
+      "price": "$5.99",
+      "category": "lunch",
+      "picture": "https://example.com/lentil_soup.jpg"
+    },
+    {
+      "name": "Chicken Shawarma",
+      "price": "$9.99",
+      "category": "lunch",
+      "picture": "https://example.com/chicken_shawarma.jpg"
+    },
+    {
+      "name": "Baked salmon",
+      "price": "$12.99",
+      "category": "dinner",
+      "picture": "https://example.com/baked_salmon.jpg"
+      },
+      {
+      "name": "Eggs Benedict",
+      "price": "$10.99",
+      "category": "breakfast",
+      "picture": "https://example.com/eggs_benedict.jpg"
+      },
+      {
+      "name": "French toast",
+      "price": "$7.99",
+      "category": "breakfast",
+      "picture": "https://example.com/french_toast.jpg"
+      },
+      {
+      "name": "Omelette",
+      "price": "$8.99",
+      "category": "breakfast",
+      "picture": "https://example.com/omelette.jpg"
+      },
+      {
+      "name": "Mimosa",
+      "price": "$5.99",
+      "category": "wine",
+      "picture": "https://example.com/mimosa.jpg"
+      },
+      {
+      "name": "Cabernet Sauvignon",
+      "price": "$29.99",
+      "category": "wine",
+      "picture": "https://example.com/cabernet_sauvignon.jpg"
+      },
+      {
+      "name": "Pinot Grigio",
+      "price": "$24.99",
+      "category": "wine",
+      "picture": "https://example.com/pinot_grigio.jpg"
+      },
+      {
+      "name": "Chardonnay",
+      "price": "$19.99",
+      "category": "wine",
+      "picture": "https://example.com/chardonnay.jpg"
+      }
+      ]
